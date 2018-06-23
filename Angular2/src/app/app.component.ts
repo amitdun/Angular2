806f3be70c613e431cb8fb42ad6d190a5bdb0ca2
Amit K Arya <amitkumar.2342@gmail.com>
2018-06-23 14:44:12 +0530

    Angular interpolation and styling Interpolation is all about data binding

--- Angular2/src/app/app.component.ts
+++ Angular2/src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 export class AppComponent  {
     //name = 'Angular 2!';
     pageHeader: string = 'Employee Details';
+    firstName: string = "Amit";
+    lastName: string = "Arya";
+    imagePath: string = "https://www.google.co.in/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png";
+    getFullName(): string {
+
+        return this.firstName + " " + this.lastName;
+    }
+
 }
