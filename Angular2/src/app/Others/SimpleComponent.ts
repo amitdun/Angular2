c7e02b8ada92228c11abf94929641dac04ec47a0
Amit K Arya <amitkumar.2342@gmail.com>
2018-07-09 22:53:31 +0530

    angular2 routing and service

---
+++ Angular2/src/app/Others/SimpleComponent.ts
@@ -0,0 +1,27 @@
+﻿import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+@Component({
+    selector: 'simple',
+    template: `You entered : {{simpleInput}}`
+})
+export class SimpleComponent implements OnChanges {
+    // Input property. As and when this property changes
+    // ngOnChanges life cycle hook method is called
+    @Input() simpleInput: string;
+
+    // Step 3 : Implementation for the hook method
+    // This code logs the current and previous value
+    // to the console.
+    ngOnChanges(changes: SimpleChanges) {
+        for (let propertyName in changes) {
+            let change = changes[propertyName];
+            let current = JSON.stringify(change.currentValue);
+            let previous = JSON.stringify(change.previousValue);
+            console.log(propertyName + ': currentValue = '
+                + current + ', previousValue = ' + previous);
+            // The above line can be rewritten using
+            // placeholder syntax as shown below
+            // console.log(`${propertyName}: currentValue
+            // = ${current }, previousValue = ${previous }`);
+        }
+    }
+}
---
+++ Angular2/src/app/employee/employeeList.component.ts
@@ -0,0 +1,46 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { IEmployee } from './employee';
+import { EmployeeService } from './employee.service';
+
+@Component({
+    selector: 'list-employee',
+    templateUrl: 'app/employee/employeeList.component.html',
+    styleUrls: ['app/employee/employeeList.component.css'],
+    
+})
+export class EmployeeListComponent  implements OnInit{
+    employees: IEmployee[];
+    selectedEmployeeCountRadioButton: string = 'All';
+    statusMessage: string = 'Loading data. Please wait...';
+    constructor(private _employeeService:EmployeeService) {
+    }
+    ngOnInit() {
+        this._employeeService.getEmployees()
+            .subscribe(employeesData => this.employees = employeesData,
+            error => {
+                console.error(error);
+                this.statusMessage = "Problem with the service. Please try again after sometime";
+            });
+    }
+    //getEmployees() {
+    //    this.employees= this._employeeService.getEmployees2();
+    //}
+   
+    trackByEmpCode(index: number, employee: any): string {
+        return employee.code;
+    }
+    getTotalEmployeeCount(): number {
+        return this.employees.length;
+    }
+    getMaleEmployeeCount(): number {
+        return this.employees.filter(x => x.gender === 'Male').length;
+    }
+    getFemaleEmployeeCount(): number {
+        return this.employees.filter(x => x.gender === 'Female').length;
+    }
+
+    onEmployeeCountRadioButtonChange(selectedRadioButtonValue: string): void {
+        this.selectedEmployeeCountRadioButton = selectedRadioButtonValue;
+        console.log(selectedRadioButtonValue);
+    }
+}
